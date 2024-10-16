75a91b61165ced24d8afab0ac97877e07c94ea8c
Aleksey Demin <matrixdemka@yandex.ru>
2024-10-16 22:49:51 +0300

    pc commit

--- translators.py
+++ translators.py
@@ -2,13 +2,14 @@ import requests
 import json
 from pprint import pp
 
+word = "red"
+ya_token = "dict.1.1.20241016T152324Z.3a8754097cfb6bdf.92b7c310f5f3015b4f28474957f0873a7e8e0390"
+translate_word = []
+url = f"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={ya_token}&lang=en-ru&text={word}"
 
-host: https://dictionary.yandex.net/api/v1/dicservice.json/lookup
-params = 
-body = 
-
-{
-    "key" : "dict.1.1.20241016T152324Z.3a8754097cfb6bdf.92b7c310f5f3015b4f28474957f0873a7e8e0390",
-    "lang" : "re-en"
-    "text" : ""
-}
+response = requests.get(url)
+data = json.loads(response.text)
+for i in data["def"]:
+    for res in i["tr"]:
+        translate_word.append(res["text"])
+return translate_word
