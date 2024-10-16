8d492fc06da370b0057892caadc2664fa04c7591
Aleksey Demin <matrixdemka@yandex.ru>
2024-10-16 21:27:20 +0300

    go work

--- translators.py
+++ translators.py
@@ -1,3 +1,14 @@
 import requests
 import json
 from pprint import pp
+
+
+host: https://dictionary.yandex.net/api/v1/dicservice.json/lookup
+params = 
+body = 
+
+{
+    "key" : "dict.1.1.20241016T152324Z.3a8754097cfb6bdf.92b7c310f5f3015b4f28474957f0873a7e8e0390",
+    "lang" : "re-en"
+    "text" : ""
+}
