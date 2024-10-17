82ea7e82de3f4194c432454de60af7e47b121ac3
Aleksey Demin <matrixdemka@yandex.ru>
2024-10-17 19:08:33 +0300

    function complete

--- translators.py
+++ translators.py
@@ -3,14 +3,28 @@ import json
 from pprint import pp
 
 
-word = "red"
-ya_token = "dict.1.1.20241016T152324Z.3a8754097cfb6bdf.92b7c310f5f3015b4f28474957f0873a7e8e0390"
-translate_word = []
-url = f"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={ya_token}&lang=en-ru&text={word}"
+url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup"
+token = "dict.1.1.20241016T152324Z.3a8754097cfb6bdf.92b7c310f5f3015b4f28474957f0873a7e8e0390"
 
-response = requests.get(url)
-data = json.loads(response.text)
-for i in data["def"]:
-    for res in i["tr"]:
-        translate_word.append(res["text"])
-return translate_word
+def translate_word(word):
+    trans_word = []
+    params = {
+        "key": token,
+        "lang": "ru-en",
+        "text": word,
+        "ui": "ru"
+    }
+
+    response = requests.get(url=url, params=params)
+    data = json.loads(response.text)
+
+    for i in data["def"]:
+        for res in i["tr"]:
+            trans_word.append(res["text"])
+    trans_word = trans_word[0]
+    return trans_word
+
+
+if __name__ == "__main__":
+    word = "машина"
+    assert translate_word(word) == "car"
